2e0315646b3abc27891e41f0e01f7ffc3ff7c52d
Ethan <ethandemougin@gmail.com>
2026-02-15 18:45:08 -0500

    gitignore

---
+++ app.js
@@ -0,0 +1,24 @@
+//Setup/ similar to html
+const express = require("express")
+//Activates app variable 
+const app = express()
+const router = express.Router()
+
+//Start the web server... app.listen(portnumber, function)
+app.listen(3000, function(){
+    console.log("Listening to port 3000")
+})
+
+//Making an api using routes
+//Routes are used to handle browser requests, they look like urls
+//The difference is when a browser requests a route, it is dynamcially handled using a function.
+
+//GET or regular request when someone goes to http://localhost:3000/hello
+app.get("/hello", function(req,res){
+    //When using a function in a route, we almost always have a parameter or handle a response and request.
+    res.send("<h1>Hello Express</h1>")
+})
+
+app.get("/goodbye", function(req,res){
+    res.send("<h1>Goodbye, Express</h1>")
+})
